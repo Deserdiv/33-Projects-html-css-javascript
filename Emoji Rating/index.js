791f79e85eebb252bf17183deb1e55d6574271c6
Deserdiv <deserdiv@gmail.com>
2023-02-12 18:48:24 +0300

    projects

---
+++ Emoji Rating/index.js
@@ -0,0 +1,25 @@
+const starsEl = document.querySelectorAll(".fa-star");
+const emojisEl = document.querySelectorAll(".far");
+const colorsArr = ["red", "orange", "lightblue","lightgreen","green"];
+
+updateRating(0);
+
+starsEl.forEach((starEl, index) => {
+  starEl.addEventListener("click", () => {
+    updateRating(index);
+  });
+});
+
+function updateRating(index) {
+  starsEl.forEach((starEl, idx) => {
+    if (idx < index + 1) {
+      starEl.classList.add("active");
+    } else {
+      starEl.classList.remove("active");
+    }
+  });
+  emojisEl.forEach(emojisEl => {
+    emojisEl.style.transform = `translateX(-${index * 50}px)`
+    emojisEl.style.color = colorsArr[index]
+  })
+}
